394311318b9bedc2243943e86f7543a446e08875
Franky777shg <frengky.sihombing.777@gmail.com>
2020-11-25 16:00:21 +0700

    intro to react

---
+++ introduction_to_react/src/component/header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Header extends React.Component {
+    render() {
+        return(
+            <ul>
+                <li>Angelo</li>
+                <li>Anjas</li>
+                <li>Andhika</li>
+            </ul>
+        )
+    }
+}
+
+export default Header
