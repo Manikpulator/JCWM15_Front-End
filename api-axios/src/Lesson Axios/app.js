42f00260fe2229adbf84a7ea4cb72a7aa67360d6
Franky777shg <frengky.sihombing.777@gmail.com>
2020-11-30 09:57:32 +0700

    prepare lesson 09

---
+++ api-axios/src/Lesson Axios/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Axios from 'axios'
+
+class App extends React.Component {
+
+    render() {
+        console.log(this.state.news)
+        return (
+            <div>
+                <h1>Hello JSON SERVER</h1>
+            </div>
+        )
+    }
+}
+
+export default App
