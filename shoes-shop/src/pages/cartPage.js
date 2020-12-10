c3911e399a65929d5748354122542213959830e7
Franky777shg <frengky.sihombing.777@gmail.com>
2020-12-10 15:09:34 +0700

    CRUD operation di cart page sudah selesai

--- shoes-shop/src/pages/cartPage.js
+++ shoes-shop/src/pages/cartPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Axios from 'axios'
 import { connect } from 'react-redux'
+import { Redirect } from 'react-router-dom'
 
 import { login } from '../actions'
 
@@ -23,6 +24,7 @@ class CartPage extends React.Component {
     handleDelete = (index) => {
         // console.log(index)
         let tempCart = this.props.cart
+        // syntax splice untuk menghapus
         tempCart.splice(index, 1)
         console.log(tempCart)
 
@@ -37,8 +39,46 @@ class CartPage extends React.Component {
             .catch((err) => console.log(err))
     }
 
-    handleDone = () => {
-        console.log('handle done')
+    handleMinus = () => {
+        if (this.state.newQty <= 0) return
+
+        this.setState({ newQty: this.state.newQty - 1 })
+    }
+
+    changeQty = (e) => {
+        this.setState({ newQty: e.target.value })
+    }
+
+    handleDone = (index) => {
+        // mengganti data pesanan suatu produk berdasarkan index
+        // tempProduct adalah tempat penyimpanan sementara data product yang ingin diedit
+        let tempProduct = this.props.cart[index]
+        // mengganti data cart untuk product yang ingin diganti
+        tempProduct.qty = parseInt(this.state.newQty)
+        tempProduct.total = this.state.newQty * this.props.cart[index].price
+        console.log(tempProduct)
+
+        // memasukan object pesanan product yang baru, ke dalam array cart
+        // tempCart adalah tempat penampungan sementara data cart user yang lama
+        let tempCart = this.props.cart
+        // syntax splice untuk mereplace
+        tempCart.splice(index, 1, tempProduct)
+        console.log(tempCart)
+
+        // mengirim perubahan ke database json
+        Axios.patch(`http://localhost:2000/users/${localStorage.id}`, { cart: tempCart })
+            .then((res) => {
+                console.log(res.data)
+
+                // update data di redux
+                Axios.get(`http://localhost:2000/users/${localStorage.id}`)
+                    .then((res) => {
+                        this.props.login(res.data)
+                        this.setState({ selectedIndex: null})
+                    })
+                    .catch((err) => console.log(err))
+            })
+            .catch((err) => console.log(err))
     }
 
     renderTHead = () => {
@@ -71,15 +111,21 @@ class CartPage extends React.Component {
                                     <Image style={{ width: 100, height: 100 }} src={item.image} rounded />
                                 </td>
                                 <td style={{ textAlign: "center" }}>{item.size}</td>
-                                <td style={{ textAlign: "center" }}>{item.price}</td>
-                                <td style={{ textAlign: "center", display: 'flex' }}>
-                                    <Button>-</Button>
-                                    <Form.Control style={{width: '50px'}} placeholder={this.state.newQty} />
-                                    <Button>+</Button>
+                                <td style={{ textAlign: "center" }}>IDR {item.price.toLocaleString()}</td>
+                                <td style={{}}>
+                                    <div style={{ display: 'flex', justifyContent: 'center' }}>
+                                        <Button onClick={this.handleMinus}>
+                                            <i className="fas fa-minus"></i>
+                                        </Button>
+                                        <Form.Control style={{ width: '100px' }} onChange={(e) => this.changeQty(e)} value={this.state.newQty} min={0} />
+                                        <Button onClick={() => this.setState({ newQty: parseInt(this.state.newQty) + 1 })}>
+                                            <i className="fas fa-plus"></i>
+                                        </Button>
+                                    </div>
                                 </td>
-                                <td style={{ textAlign: "center" }}>{item.total}</td>
+                                <td style={{ textAlign: "center" }}>IDR {(this.state.newQty * item.price).toLocaleString()}</td>
                                 <td style={{ textAlign: "center" }}>
-                                    <Button variant='success' onClick={this.handleDone} style={{ marginRight: '15px' }}>Done</Button>
+                                    <Button variant='success' onClick={() => this.handleDone(index)} style={{ marginRight: '15px' }}>Done</Button>
                                     <Button variant='danger' onClick={() => this.setState({ selectedIndex: null })}>Cancel</Button>
                                 </td>
                             </tr>
@@ -93,9 +139,9 @@ class CartPage extends React.Component {
                                 <Image style={{ width: 100, height: 100 }} src={item.image} rounded />
                             </td>
                             <td style={{ textAlign: "center" }}>{item.size}</td>
-                            <td style={{ textAlign: "center" }}>{item.price}</td>
+                            <td style={{ textAlign: "center" }}>IDR {item.price.toLocaleString()}</td>
                             <td style={{ textAlign: "center" }}>{item.qty}</td>
-                            <td style={{ textAlign: "center" }}>{item.total}</td>
+                            <td style={{ textAlign: "center" }}>IDR {item.total.toLocaleString()}</td>
                             <td style={{ textAlign: "center" }}>
                                 <Button variant='warning' onClick={() => this.setState({ selectedIndex: index, newQty: item.qty })} style={{ marginRight: '15px' }}>Edit</Button>
                                 <Button variant='danger' onClick={() => this.handleDelete(index)}>Delete</Button>
@@ -108,8 +154,11 @@ class CartPage extends React.Component {
     }
 
     render() {
+        if(!this.props.id) return <Redirect to='/login'/>
+        
         // console.log(this.props.cart)
-        console.log(this.state.selectedIndex)
+        // console.log(this.state.selectedIndex)
+        // console.log(this.state.newQty)
         return (
             <div style={{ marginTop: '70px', padding: '0 15px' }}>
                 <h1>Ini Cart Page</h1>
@@ -124,7 +173,8 @@ class CartPage extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
-        cart: state.user.cart
+        cart: state.user.cart,
+        id: state.user.id
     }
 }
 
